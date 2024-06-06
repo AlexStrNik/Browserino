e4686e6ea6d4e86d06a911812a7e6a702b2075f6
alexstrnik <alex.str.nik@gmail.com>
2024-06-06 10:18:14 +0300

    Initial Commit

---
+++ Browserino/BrowserinoApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrowserinoApp.swift
+//  Browserino
+//
+//  Created by Aleksandr Strizhnev on 06.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct BrowserinoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Browserino/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Browserino
+//
+//  Created by Aleksandr Strizhnev on 06.06.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
